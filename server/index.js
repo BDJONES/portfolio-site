903b09515fb2e0d8def3d9af0849cb2c7b9bc6d0
Brandon Jones <coalmander.afterdark@gmail.com>
2026-02-04 20:21:23 -0500

    Tried to push path up one level

--- server/index.js
+++ server/index.js
@@ -149,7 +149,7 @@ app.get('/api/health', (req, res) => {
 app.use(express.static(distPath));
 
 // Explicit root route handler
-app.get('/', (req, res) => {
+app.get('../', (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
 });
 
