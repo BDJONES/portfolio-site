1e4b48830c70b068a54319660a6003349ebf2272
Brandon Jones <coalmander.afterdark@gmail.com>
2026-02-05 21:06:15 -0500

    Fixed issues with accent at top of job card

--- frontend/src/components/Home/HomeText.tsx
+++ frontend/src/components/Home/HomeText.tsx
@@ -8,10 +8,14 @@ function HomeText() {
                 // backgroundColor: "rgba(254,240,229,255)",
                 // borderRadius: 4,
             }}
-            className="TextContainer shadow-lg shadow-slate-950 p-2 border-1 border-slate-700 bg-slate-800 "
+            className="TextContainer shadow-lg shadow-slate-950 p-2 border border-slate-700 bg-slate-800 "
         >
-            <p className="text-slate-100 text-2xl min-[431px]:text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-center font-bold">Brandon Jones</p>
-            <p className="text-violet-400 text-base min-[431px]:text-lg sm:text-xl md:text-2xl lg:text-3xl text-center font-lora">Software Engineer</p>
+            <p className="text-slate-100 text-2xl min-[431px]:text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-center font-bold">
+                Brandon Jones
+            </p>
+            <p className="text-violet-400 text-base min-[431px]:text-lg sm:text-xl md:text-2xl lg:text-3xl text-center font-lora">
+                Software Engineer
+            </p>
         </Box>
     );
 }
