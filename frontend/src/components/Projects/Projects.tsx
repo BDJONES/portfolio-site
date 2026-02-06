db230578a9dd444417774fff36ef92d6d50b4f7b
Brandon Jones <coalmander.afterdark@gmail.com>
2026-02-05 21:15:53 -0500

    Updated project story for Pokemon Game

--- frontend/src/components/Projects/Projects.tsx
+++ frontend/src/components/Projects/Projects.tsx
@@ -14,7 +14,7 @@ const projects: ProjectProps[] = [
             "/images/project_assets/Pokemon_Battle_Evolution/PokemonThumbnail.png",
         description:
             "A Pokemon Battle Evolution battle simulator built with Unity. This game allows you to battle your friends online, competeting in highstakes battles with your favorite Pokemon.",
-        story: "This project was a passion project that I built in my free time. I wanted to create a game that would be fun to play and would challenge my skills as a developer. I used Unity to build the game and I used the Unity Editor to build the game. I used the Unity Engine to build the game. I used the Unity API to build the game. I used the Unity SDK to build the game. I used the Unity API to build the game. I used the Unity SDK to build the game. I used the Unity API to build the game. I used the Unity SDK to build the game.",
+        story: "This project was a passion project that I built in my free time. I wanted to create a game that would be fun to play and would challenge my skills as a developer. I used Unity to build the game and I used the Unity Cloud Services to enable multiplayer connectivity",
         tech: ["Unity", "C#", "Blender", "GitHub", "Unity Cloud Services"],
         media: [
             {
